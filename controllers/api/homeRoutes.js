4e596cb46db91155d4f2dbc0d6fe78a03db0f32b
karlnh <hubigkarl@gmail.com>
2023-02-13 23:01:52 -0600

    create basic REST route setup

---
+++ controllers/api/homeRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+
+router.get('/' async (req, res) => {
+    // do stuff
+})
+
+router.get('/chat', withAuth, async (req, res) => {
+    // do stuff
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        // do stuff
+    }
+    res.render('login');
+});
+
+module.exports = router;
