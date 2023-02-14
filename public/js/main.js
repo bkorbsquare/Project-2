6a83f427c6cb92c1342b9159d5a812ab56e6ce97
karlnh <hubigkarl@gmail.com>
2023-02-13 21:32:42 -0600

    add main socket script

---
+++ public/js/main.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // const usernameInput = document.querySelector('.username');
+    const messages = require('../../views/chat.handlebars') || document.querySelector('.messages');
+    const loginInfo = require('./login');
+
+    const socket = io();
+
+    let username
+    let connected = false;
+
+    const setUsername = () => {
+        username = loginInfo.loginFormHandler;
+        if (username) { // if username is valid
+            // switch to chat page somehow?
+            socket.emit('add user', username);
+        };
+    };
+
+    const sendMessage = (data) => {
+        let message = data;
+        if (message && connected) {
+            addChatMessage(username, message);
+            socket.emit('new message', message);
+        };
+    };
+
+    const addChatMessage = (data) => {
+        const usernameDiv = `<span class="username">${data.username}</span>`;
+        const messageDiv = `<span class="messageBody">${data.message}</span>`;
+        if (!usernameDiv || !messageDiv) {
+            console.log("There is a problem in the input data.");
+        } else {
+            const chatDiv = `<li class="chatItem"><strong>${usernameDiv}:</strong> ${messageDiv}</li>`;
+        };
+    };
+
+    // Socket events
+    socket.on('login', (data) => {
+        connected = true;
+        // addJoinMessage(data);
+    });
+
+    socket.on('new chat', (data) => {
+        addChatMessage(data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('You have been disconnected.');
+    });
+
+    socket.io.on('reconnect', () => {
+        console.log('You have reconnected.');
+        if (username) {
+            socket.emit('add user', username);
+        }
+    });
+
+    socket.io.on('reconnect_error', () => {
+        console.log('Reconnect attempt failed.');
+    });
+});
