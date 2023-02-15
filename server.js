e465a6d041e5f0db2b9fc9b17b8f1bfc5df2e363
Brandon <110788407+bkorbsquare@users.noreply.github.com>
2023-02-14 19:32:12 -0600

    Homeroutes & Server.js

--- server.js
+++ server.js
@@ -5,9 +5,33 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 3001;
+const handlebars = require('express-handlebars');
+
+const hbs = handlebars.create({});
 
 // Default Routing Page
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+  app.use(session(sess));
 
 server.listen(PORT, () => {
     console.info(`Listening on ${PORT}`);
