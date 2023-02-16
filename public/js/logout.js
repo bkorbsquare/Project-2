b56aa70166fb6be27194f0420827ade29cd34836
karlnh <hubigkarl@gmail.com>
2023-02-15 20:54:30 -0600

    refactor and rename

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,19 @@
-const logout = async () => {
-    const response = await fetch('/api/users/logout', {
+const logoutHandler = async () => {
+  console.log("Logout attempted.");
+
+  const response = await fetch('api/users/logout', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
+      headers: { 'Content-Type': 'applications/json' },
+  });
+
+  if (response.ok) {
+      console.log("Logout response successful.");
       document.location.replace('/');
-    } else {
-      alert('Failed to log out.');
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+  } else {
+      alert('Failed to logout.');
+  }
+};
+
+document
+  .querySelector('.logout-btn')
+  .addEventListener('click', logoutHandler);
