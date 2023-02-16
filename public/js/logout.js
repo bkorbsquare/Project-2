48ce4dd6d6fbb60ab879af5f66bb856eb628ee8b
karlnh <hubigkarl@gmail.com>
2023-02-16 00:17:32 -0600

    standardize handler name; add logs

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,19 @@
-const logout = async () => {
+const logoutHandler = async () => {
+  console.log("Logout attempted.");
+
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
   
     if (response.ok) {
+      console.log("Logout response successful.");
       document.location.replace('/');
     } else {
       alert('Failed to log out.');
     }
   };
   
-  document.querySelector('#logout').addEventListener('click', logout);
+  document
+  .querySelector('#logout')
+  .addEventListener('click', logoutHandler);
