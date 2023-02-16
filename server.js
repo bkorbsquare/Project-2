a904f81ebd7057839e48eb24d2dcd8e50637e84e
karlnh <hubigkarl@gmail.com>
2023-02-16 00:10:12 -0600

    remove socket code

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
+const clog = require('clog');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -36,50 +37,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    server.listen(PORT, () => {
-        console.info(`Listening on ${PORT}`);
+// Socket.io
+io.on('connection', (socket) => {
+    clog.info(`Connecting into ${socket}.`);
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
     });
 });
 
-// // On connect
-// io.on('connect', (socket) => {
-//     console.log(`Connection into ${socket}.`);
 
-//     // if user added:
-//     socket.on('add user', (username) => {
-//         // changing socket username to new user
-//         socket.username = username;
-//         // local emit for login, not global
-//         socket.emit('login', {
-//             username: socket.username
-//         }),
-//         // global emit for user join
-//         socket.broadcast.emit('user joined', {
-//             username: socket.username
-//         });
-//         console.log(`${username} joined.`);
-//     });
+sequelize.sync({force: false}) // UNDO THIS LATER
+.then(() => {
+    app.listen(PORT, () => {
+        clog.info(`Now listening on ${PORT}!`);
+    });
 
-//     // if (new) chat sent:
-//     socket.on('chat', (data) => {
-//         if (!data || data === "") {
-//             console.log("No chat data present to send.");
-//         } else {
-//             socket.broadcast.emit('new message', {
-//                 username: socket.username,
-//                 message: data
-//             });
-//             console.log(`Chat data sent to ${socket}.`);
-//         };
-//     });
-
-//     // on user disconnect
-//     socket.on('disconnect', () => {
-//         // global emit that client has left
-//         socket.broadcast.emit('user left', {
-//             username: socket.username
-//         });
-//         console.log(`Disconnect from ${socket}.`);
-//     });
-// });
+    http.listen(3000, () => {
+        clog.info(`Testing at 3000`);
+    });
+});
